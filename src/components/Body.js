b8c31d3d41fe330709f3f5e34547585f771445d4
Abhay Aswal <abhayaswal939@gamil.com>
2024-03-11 14:30:42 +0530

    update

--- src/components/Body.js
+++ src/components/Body.js
@@ -27,6 +27,7 @@ const Body = ()=>{
     getRestaurants();
   },[])
 
+{
   async function getRestaurants(){
     const data= await fetch(
 "https://www.swiggy.com/dapi/restaurants/list/v5?lat=29.74750&lng=78.52840&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
@@ -37,6 +38,7 @@ const Body = ()=>{
     setAllRestaurnats(json?.data?.cards[2]?.data?.data?.cards);
     setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
   }
+}
 
 
 
@@ -84,76 +86,3 @@ const Body = ()=>{
 
 export default Body;
 
-// import { useState, useEffect } from "react";
-// import  restaurantList  from "../Constants";
-// import RestaurantCard from "./RestaurantCard";
-// import Shimmer from "./Shimmer";
-
-// function filterData(searchText, restaurants) {
-//   const filterdata = restaurants.filter((restaurant) =>
-//     restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
-//   );
-//   return filterdata;
-// }
-
-// const Body = () => {
-//   // const searchText = "KFC";
-//   const [allRestaurants, setAllRestaurants] = useState([]);
-//   const [filteredrestaurants, setFilteredRestaurants] = useState([]);
-//   const [searchText, setSearchText] = useState("");
-//   // to maintain the list of restaurant
-
-//   useEffect(() => {
-//     //API CAll
-//     getRestaurants();
-//     console.log("useEffect");
-//   }, []);
-
-//   async function getRestaurants() {
-//     const data = await fetch(
-//       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.8779714&lng=75.3488766&page_type=DESKTOP_WEB_LISTING"
-//     ); //this will return readable string will convert this readable stringto json
-//     const json = await data.json();
-//     setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
-//     setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
-//   }
-
-//   console.log("render");
-
-//   if (!allRestaurants) return null;
-
-//   //conditional Rendering
-//   // if restaurant is empty => Shimmer UI
-//   // if data is there => actual data
-//   return allRestaurants.length === 0 ? (
-//     <Shimmer />
-//   ) : (
-//     <>
-//       <div className="search-container">
-//         <input
-//           type="text"
-//           className="search-input"
-//           placeholder="Search"
-//           value={searchText}
-//           onChange={(e) => setSearchText(e.target.value)}
-//         />
-//         <button
-//           className="search-btn"
-//           onClick={() => {
-//             const data = filterData(searchText, allRestaurants);
-//             setFilteredRestaurants(data);
-//           }}
-//         >
-//           Search
-//         </button>
-//       </div>
-//       <div className="restaurnant-list">
-//         {filteredrestaurants.map((restaurant) => {
-//           return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />;
-//         })}
-//       </div>
-//     </>
-//   );
-// };
-// export default Body;
-//   This file is names as Body.jsx beacause it's react file we can write .js no issues or if code in typescript we can write .tsx or .ts
